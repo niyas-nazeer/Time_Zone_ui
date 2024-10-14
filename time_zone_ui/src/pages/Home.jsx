5e61dfb87de1600f025ac2aca80bb93109aad862
niyas-nazeer <niyasnazeer719@gmail.com>
2024-10-14 11:24:53 +0530

    Add login feature

---
+++ time_zone_ui/src/pages/Home.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useEffect } from 'react'
+
+const Home = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem('access');
+
+    // Function to verify the token
+    const verifyToken = async () => {
+      if (!accessToken) {
+        navigate('/login');
+        return; // Exit if there's no token
+      }
+
+      try {
+        const data = { access: accessToken };
+        const response = await fetch('http://127.0.0.1:8000/api/user/verify', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization':'Bearer '+accessToken
+          },
+          body: JSON.stringify(data),
+        });
+
+        // Check if the response is OK
+        if (!response.ok) {
+            localStorage.removeItem('refresh');
+            localStorage.removeItem('access');
+            localStorage.removeItem('userType');
+          navigate('/login');
+        }
+
+      } catch (err) {
+        // console.error(err);
+      }
+    };
+
+    verifyToken(); // Call the async function
+
+  }, []); 
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
