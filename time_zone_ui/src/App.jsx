a3121d2ef9fa790f6b16a0d0e26e0a8ec860c849
niyas-nazeer <niyasnazeer719@gmail.com>
2024-10-14 11:24:53 +0530

    Add login feature

---
+++ time_zone_ui/src/App.jsx
@@ -0,0 +1,16 @@
+import Login from './pages/Login'
+import Home from './pages/Home'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+  </Router>
+  )
+}
+
+export default App
