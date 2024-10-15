45cd21364f50deefcd3792dbf2aa9fb61b483465
niyas-nazeer <niyasnazeer719@gmail.com>
2024-10-15 12:55:57 +0530

    Add nav component

--- time_zone_ui/src/App.jsx
+++ time_zone_ui/src/App.jsx
@@ -2,6 +2,7 @@ import Login from './pages/Login'
 import Home from './pages/Home'
 import Register from './pages/RegistrationForm'
 import Product from './components/Product'
+import NavBar from './components/NavBar'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home />} />
         <Route path="/product" element={<Product/>}/>
+        <Route path="/navbar" element={<Home/>}/>
       </Routes>
   </Router>
   )
