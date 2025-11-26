68f072a5760d87236f20fd658fc3d0513756891c
niyas-nazeer <niyasnazeer719@gmail.com>
2025-11-26 14:54:52 +0530

    Men page created

--- time_zone_ui/src/App.jsx
+++ time_zone_ui/src/App.jsx
@@ -1,8 +1,7 @@
 import Login from './pages/Login'
 import Home from './pages/Home'
+import Men from './pages/Men'
 import Register from './pages/RegistrationForm'
-import Product from './components/Product'
-import NavBar from './components/NavBar'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -12,8 +11,7 @@ function App() {
         <Route path="/signup" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home />} />
-        <Route path="/product" element={<Product/>}/>
-        <Route path="/navbar" element={<Home/>}/>
+        <Route path="/men" element={<Men />} />
       </Routes>
   </Router>
   )
