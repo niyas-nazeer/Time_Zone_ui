7bed37d67c2174a10c67df196f1da82b7853d0da
niyas-nazeer <niyasnazeer719@gmail.com>
2025-12-01 13:13:10 +0530

    Offers page created

--- time_zone_ui/src/App.jsx
+++ time_zone_ui/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home'
 import Men from './pages/Men'
 import Women from './pages/Women'
 import Brands from './pages/Brands'
+import Offers from './pages/Offers'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
         <Route path="/men" element={<Men />} />
         <Route path="/women" element={<Women />} />
         <Route path="/brands" element={<Brands />} />
+        <Route path="/offers" element={<Offers />} />
       </Routes>
   </Router>
   )
